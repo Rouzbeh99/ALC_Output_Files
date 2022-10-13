d0373b6735e33fac8d0ea786a7d03e2ba47f4b73
Rouzbeh99 <rouzbehpaktinat99@gmail.com>
2022-10-13 12:22:11 -0600

    updated

--- script.py
+++ script.py
@@ -1,14 +1,6 @@
 number1 = int(input("number1: "))
 number2 = int(input("number2: "))
 
-if number1 > number2:
-    greater = number1
-    smaller = number2
-else:
-    greater = number2
-    smaller = number1
+result = number1/number2
 
-result = (greater - smaller) / greater * 100
-
-print(result)
-print("improvement") if number1 > number2  else print("degradation")
+print(result,"X")
